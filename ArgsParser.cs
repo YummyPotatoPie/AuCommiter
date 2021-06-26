32aa711595adaf7a6a6919cf395717fab1722607
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-06-26 11:32:29 +0300

    cats are badass

---
+++ ArgsParser.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace AuCommiter
+{
+    internal static class ArgsParser
+    {
+        public static Dictionary<string, string> Parse(string argsString)
+        {
+            return new Dictionary<string, string> { };
+        }
+    }
+}
---
+++ Commiter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace AuCommiter
+{
+    class Commiter
+    {
+        static void Main(string[] args)
+        {
+            string argsString = "";
+            foreach (string arg in args)
+                argsString += arg;
+
+            Dictionary<string, string> parsedArgs = ArgsParser.Parse(argsString);
+        }
+    }
+}
